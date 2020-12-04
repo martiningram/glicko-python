83507f5adcd162c77345331c17f51a2c40a35f6a
Martin Ingram <martin.ingram@gmail.com>
2020-12-04 11:29:45 +1100

    First attempt at adding constant-variance version

---
+++ glicko/constant_variance_glicko.py
@@ -0,0 +1,83 @@
+from .glicko import calculate_new_rating, calculate_win_prob
+from collections import defaultdict
+
+
+def calculate_ratings(winners, losers, variance):
+
+    ratings = defaultdict(lambda: 1500.0)
+
+    total_discrepancy = 0.0
+
+    history = list()
+
+    for cur_winner, cur_loser in zip(winners, losers):
+
+        winner_rating, loser_rating = ratings[cur_winner], ratings[cur_loser]
+
+        new_mean_winner, _, discrepancy = calculate_new_rating(
+            winner_rating, variance, loser_rating, variance, 1, 1
+        )
+
+        # No need for discrepancy here since we would be double-counting the outcome
+        new_mean_loser, _, _ = calculate_new_rating(
+            loser_rating, variance, winner_rating, variance, 1, 0
+        )
+
+        total_discrepancy += discrepancy
+
+        ratings[cur_winner] = new_mean_winner
+        ratings[cur_loser] = new_mean_loser
+
+        history.append(
+            {
+                "prior_mu_winner": winner_rating,
+                "prior_mu_loser": loser_rating,
+                "prior_win_prob": calculate_win_prob(
+                    winner_rating, loser_rating, variance, variance
+                ),
+            }
+        )
+
+    return ratings, history, total_discrepancy
+
+
+def find_optimal_parameters(
+    winners, losers, start_variance=100 ** 2, verbose=True, tolerance=1e-2,
+):
+    """Fits the parameters in constant-variance Glicko.
+
+    Args:
+    winners: The array of winners for each of N contests, of shape [N,].
+    losers: The array of losers for each of N contests, of shape [N,].
+    start_variance: The initialisation for the fixed variance for each player in
+        the optimisation.
+    verbose: Whether or not to print the progress of the optimisation.
+    tolerance: The tolerance required for the optimisation to successfully
+        terminate.
+    
+    Returns:
+    A Tuple whose first entry is a flag indicating the optimisation's success,
+    and the second is a dictionary containing one element: the optimal prior
+    variance.
+    """
+
+    def fun_to_minimize(theta):
+
+        # Constrain
+        variance = theta ** 2
+
+        _, _, discrepancy = calculate_ratings(winners, losers, variance)
+
+        if verbose:
+            print(
+                f"Player standard deviation: {np.sqrt(variance):.2f}; "
+                f"discrepancy: {discrepancy:.2f}"
+            )
+
+        return discrepancy
+
+    opt_result = minimize(fun_to_minimize, np.sqrt([start_variance]), tol=tolerance,)
+
+    fit_x = opt_result.x ** 2
+
+    return (opt_result.success, {"variance": fit_x[0]})
