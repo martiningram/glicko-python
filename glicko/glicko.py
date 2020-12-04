Fix calculation of discrepancy

glicko/glicko.py
@@ -147,6 +147,17 @@ def calculate_ratings(
         cur_winners = winners[periods == cur_period]
         cur_losers = losers[periods == cur_period]
 
+        # Calculate the discrepancy before updating:
+        for cur_winner, cur_loser in zip(cur_winners, cur_losers):
+
+            winner_mu, winner_sigma_sq = prior_ratings[cur_winner]
+            loser_mu, loser_sigma_sq = prior_ratings[cur_loser]
+            cur_sij = 1.0
+
+            total_discrepancy += calculate_discrepancy(
+                winner_mu, loser_mu, winner_sigma_sq, loser_sigma_sq, cur_sij
+            )
+
         unique_players = np.union1d(cur_winners, cur_losers)
 
         for cur_unique_player in unique_players:
@@ -193,7 +204,7 @@ def calculate_ratings(
             # We can now update the player
             player_mu, player_sigma_sq = prior_ratings[cur_unique_player]
 
-            new_mu, new_sigma_sq, discrepancy = calculate_new_rating(
+            new_mu, new_sigma_sq, _ = calculate_new_rating(
                 player_mu,
                 player_sigma_sq,
                 opponent_means,
@@ -201,8 +212,6 @@ def calculate_ratings(
                 n_j,
                 s_jk,
             )
-
-            total_discrepancy += discrepancy
 
             new_ratings[cur_unique_player] = (new_mu, new_sigma_sq)
 

glicko/helpers.py
@@ -4,8 +4,8 @@ from .glicko import calculate_win_prob
 from datetime import timedelta
 
 
-def predict_win_probabilities(players, opponents, periods, ratings_history):
-    """Predicts the win probabilities for players given Glicko ratings.
+def predict_win_probabilities_and_ratings(players, opponents, periods, ratings_history):
+    """Predicts the win probabilities and ratings for players given Glicko ratings.
 
     Args:
     players: A numpy array of player names.
@@ -14,9 +14,9 @@ def predict_win_probabilities(players, opponents, periods, ratings_history):
     ratings_history: The list of ratings returned by running
         glicko.calculate_ratings.
     
-    Returns:
-    An array of win probabilities with one entry for each of the specified
-    matches.
+    Returns: 
+    A DataFrame containing win probabilities and prior ratings with one
+    entry for each of the specified matches.
     """
 
     preds = list()
@@ -27,10 +27,18 @@ def predict_win_probabilities(players, opponents, periods, ratings_history):
         opponent_mean, opponent_var = ratings_history[cur_period][cur_opponent]
 
         preds.append(
-            calculate_win_prob(player_mean, opponent_mean, player_var, opponent_var)
+            {
+                "player_mean": player_mean,
+                "player_var": player_var,
+                "opponent_mean": opponent_mean,
+                "opponent_var": opponent_var,
+                "win_prob": calculate_win_prob(
+                    player_mean, opponent_mean, player_var, opponent_var
+                ),
+            }
         )
 
-    preds = np.array(preds)
+    preds = pd.DataFrame(preds)
 
     return preds
 